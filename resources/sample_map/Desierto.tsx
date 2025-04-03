d559fe483e817a0d521b02a4460faa871c94938d
Pablo Tordesillas <pablo.tordesillas@edu.uah.es>
2025-04-03 14:05:11 +0200

    Mapa de ejemplo a ruinas

---
+++ resources/sample_map/Desierto.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Desierto" tilewidth="32" tileheight="32" tilecount="80" columns="8">
+ <image source="Asets pmk/Otros/Desierto.png" width="256" height="320"/>
+</tileset>
