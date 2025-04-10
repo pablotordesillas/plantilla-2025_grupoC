ea3644c5f4c058f19928b48c42e00d2df49f9e66
Emanuel Baciu <irinelsiemanuel2007@gmail.com>
2025-04-10 16:49:47 +0200

    changed spawnpoint to house next to fireplace

--- rpg/constants.py
+++ rpg/constants.py
@@ -21,9 +21,9 @@ BOTTOM_VIEWPORT_MARGIN = 300
 TOP_VIEWPORT_MARGIN = 300
 
 # What map, and what position we start at
-STARTING_MAP = "main_map"
-STARTING_X = 33
-STARTING_Y = 16
+STARTING_MAP = "farmhouse"
+STARTING_X = 13
+STARTING_Y = 4
 
 # Key mappings
 KEY_UP = [arcade.key.UP, arcade.key.W]
