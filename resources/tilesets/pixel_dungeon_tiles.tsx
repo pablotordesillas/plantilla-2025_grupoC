6c9db11cdc5bc84b2b71599bbb32e134e354a53e
Emanuel Baciu <irinelsiemanuel2007@gmail.com>
2025-04-10 16:23:40 +0200

    changed source from the files from self computer to actual docs from the project

--- resources/tilesets/pixel_dungeon_tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Pixel dungeon" tilewidth="32" tileheight="32" tilecount="442" columns="13">
- <image source="../../../../../../UAH/Cuatrimestre 2/Tecnologia de Videojuegos/Tiled maps/Projects/mapas-proyectoTDV/Cave/TilesCave/Cave/Pixel-Dungeon/PixelDungeon/Pixel dungeon..jpg" width="416" height="1088"/>
-</tileset>
