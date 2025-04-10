dfb8b8f921dfccd7827e66b2d03e535d50a63b24
Emanuel Baciu <irinelsiemanuel2007@gmail.com>
2025-04-10 15:54:25 +0200

    added .tsx files for tilesets

---
+++ resources/tilesets/cave-deco-tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="decorative-Cave" tilewidth="32" tileheight="32" tilecount="390" columns="13">
+ <image source="../../../../../../UAH/Cuatrimestre 2/Tecnologia de Videojuegos/Tiled maps/Projects/mapas-proyectoTDV/Cave/TilesCave/Cave/RPGW-Cave/RPGW_Caves_v2.1/decorative.png" width="416" height="960"/>
+</tileset>
