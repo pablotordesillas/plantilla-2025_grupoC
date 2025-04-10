6ebd2fe4285345f36024a49659dfb444748b6852
Emanuel Baciu <irinelsiemanuel2007@gmail.com>
2025-04-10 15:50:53 +0200

    added tilesets to the resources folder to check if it works

---
+++ resources/tilesets/Cave/Pixel-Dungeon/Patrones-Tiled-PixelDungeon/Pixel dungeon.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Pixel dungeon" tilewidth="32" tileheight="32" tilecount="442" columns="13">
+ <image source="../PixelDungeon/Pixel dungeon..jpg" width="416" height="1088"/>
+</tileset>
---
+++ resources/tilesets/Cave/RPGW-Cave/Patrones Tiled RPGW-Cave/decorative-Cave.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="decorative-Cave" tilewidth="32" tileheight="32" tilecount="390" columns="13">
+ <image source="../RPGW_Caves_v2.1/decorative.png" width="416" height="960"/>
+</tileset>
