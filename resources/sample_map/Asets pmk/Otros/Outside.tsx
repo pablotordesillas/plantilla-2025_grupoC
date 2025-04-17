49095343d0799bf48bb1ca327c050c2d3f4dbeee
Emanuel Baciu <irinelsiemanuel2007@gmail.com>
2025-04-17 23:06:49 +0200

    Cambiado el mapa de piramide para que tenga un camino de verdad (main-00-01-02-12-11-01-00-main-11-21-22). Solo quedaria decorar mas

---
+++ resources/sample_map/Asets pmk/Otros/Outside.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Outside" tilewidth="32" tileheight="32" tilecount="4016" columns="8">
+ <image source="Outside.png" width="256" height="16064"/>
+</tileset>
