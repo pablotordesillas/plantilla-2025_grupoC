a1f7b9e07f287a481b579684ea071947119f4b77
sasi <isaac.quintian@edu.uah.es>
2025-05-21 15:27:16 +0200

    Sonido de los pasos cambiados segun el mapa

--- rpg/sprites/player_sprite.py
+++ rpg/sprites/player_sprite.py
@@ -1,15 +1,20 @@
 import arcade
 import time
+import rpg.constants as const
 from rpg.sprites.chacter_sprite1 import CharacterSprite_one
-from rpg.sprites.character_sprite import CharacterSprite
-
 
 class PlayerSprite(CharacterSprite_one):
     def __init__(self, sheet_name, sheet_name2, sheet_name3, sheet_name4):
         super().__init__(sheet_name, sheet_name2, sheet_name3, sheet_name4)
         self.sound_update = 0
-        self.footstep_sound = arcade.load_sound(":sounds:nivel0/pasos.mp3") #cambialo reventado de mierda
-        print(self.footstep_sound)
+        self.footstep_sounds = {
+            0: arcade.load_sound(":sounds:nivel0/pasos.mp3"),
+            1: arcade.load_sound(":sounds:nivel2/pasos.mp3"),
+            2: arcade.load_sound(":sounds:nivel1/pasos.mp3"),
+            3: arcade.load_sound(":sounds:nivel0/pasos.mp3"),
+            4: arcade.load_sound(":sounds:nivel2/pasos.mp3"),
+            5: arcade.load_sound(":sounds:nivel1/pasos.mp3")
+        }
 
     def on_update(self, delta_time):
         super().on_update(delta_time)
@@ -18,7 +23,7 @@ class PlayerSprite(CharacterSprite_one):
         speed = (self.change_x ** 2 + self.change_y ** 2) ** 0.5
         self.sound_update += speed * delta_time
         if self.sound_update >= 1.0:
-            arcade.play_sound(self.footstep_sound)
+            sonido_actual = self.footstep_sounds.get(const.SONIDO)
+            if sonido_actual:
+                arcade.play_sound(sonido_actual)
             self.sound_update = 0
-
-
